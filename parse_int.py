3e94a67b3ee4b798544be9b04e43d65ac9b6903a
panky7 <pankaj.purple@gmail.com>
2018-10-16 11:12:41 +0200

    commit

---
+++ parse_int.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+import sys
+import os
+import textfsm as textfsm
+
+# Load the input file to a variable
+input_file = open("/Users/JANINE/test123/run_int")
+raw_text_data = input_file.read()
+input_file.close()
+
+# Run the text through the FSM. 
+# The argument 'template' is a file handle and 'raw_text_data' is a 
+# string with the content from the show_inventory.txt file
+template = open("/Users/JANINE/test123/interface.template")
+re_table = textfsm.TextFSM(template)
+fsm_results = re_table.ParseText(raw_text_data)
+
+# the results are written to a CSV file
+datestring = datetime.strftime(datetime.now(),'%Y-%m-%d-%H-%M')
+os.chdir(r'/Users/JANINE/test123')
+outfile_name = open("nexus_run_interface.csv", "w+")
+outfile = outfile_name
+
+# Display result as CSV and write it to the output file
+# First the column headers...
+print(re_table.header)
+for s in re_table.header:
+    outfile.write("%s;" % s)
+outfile.write("\n")
+
+# ...now all row's which were parsed by TextFSM
+counter = 0
+for row in fsm_results:
+    print(row)
+    for s in row:
+        outfile.write("%s;" % s)
+    outfile.write("\n")
+    counter += 1
+print("Write %d records" % counter)
+
+outfile_name.close()
---
+++ parse_nexus_int.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+import sys
+import os
+#sys.path.append ('C:\Python27\Lib\site-packages\.')
+import textfsm as textfsm
+
+# Load the input file to a variable
+input_file = open("/Users/JANINE/test123/nexus_int")
+raw_text_data = input_file.read()
+input_file.close()
+
+# Run the text through the FSM. 
+# The argument 'template' is a file handle and 'raw_text_data' is a 
+# string with the content from the show_inventory.txt file
+template = open("/Users/JANINE/test123/cisco_ios_show_interfaces.template")
+re_table = textfsm.TextFSM(template)
+fsm_results = re_table.ParseText(raw_text_data)
+
+# the results are written to a CSV file
+datestring = datetime.strftime(datetime.now(),'%Y%m%d%H%M%S')
+os.chdir(r'/Users/JANINE/test123')
+outfile_name = open("nexus_show_int.csv", "w+")
+outfile = outfile_name
+
+# Display result as CSV and write it to the output file
+# First the column headers...
+print(re_table.header)
+for s in re_table.header:
+    outfile.write("%s;" % s)
+outfile.write("\n")
+
+# ...now all row's which were parsed by TextFSM
+counter = 0
+for row in fsm_results:
+    print(row)
+    for s in row:
+        outfile.write("%s;" % s)
+    outfile.write("\n")
+    counter += 1
+print("Write %d records" % counter)
+
+outfile_name.close()
