6783c3f5f9e413dd2aa27fe04bcc6c9d4b2bf063
panky7 <pankaj.purple@gmail.com>
2018-10-17 10:27:00 +0200

    pim commit

--- parse_pim.py
+++ parse_pim.py
@@ -1,25 +1,24 @@
 from datetime import datetime
 import sys
 import os
-#sys.path.append ('C:\Python27\Lib\site-packages\.')
 import textfsm as textfsm
 
 # Load the input file to a variable
-input_file = open("/Users/JANINE/nexus/vrrp_hsrp")
+input_file = open("/Users/JANINE/nexus/run_int")
 raw_text_data = input_file.read()
 input_file.close()
 
 # Run the text through the FSM. 
 # The argument 'template' is a file handle and 'raw_text_data' is a 
 # string with the content from the show_inventory.txt file
-template = open("/Users/JANINE/nexus/vrrp.template")
+template = open("/Users/JANINE/nexus/pim.template")
 re_table = textfsm.TextFSM(template)
 fsm_results = re_table.ParseText(raw_text_data)
 
 # the results are written to a CSV file
-datestring = datetime.strftime(datetime.now(),'%Y%m%d%H%M%S')
+datestring = datetime.strftime(datetime.now(),'%Y-%m-%d-%H-%M')
 os.chdir(r'/Users/JANINE/nexus')
-outfile_name = open("vrrp1.csv", "w+")
+outfile_name = open("pim_interface.csv", "w+")
 outfile = outfile_name
 
 # Display result as CSV and write it to the output file
